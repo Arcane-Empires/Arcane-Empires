ca69c3f2d59030471e14b46c8159984a1c2eeb14
LenBanana <lenstandvoss@yahoo.de>
2024-09-28 03:30:16 +0200

    Serve frontend from nodejs backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import authRoutes from './routes/auth.js'; // Adjust the path as necessary
 import protectedRoute from './routes/protectedRoute.js'; // Adjust the path as necessary
+import path from 'path';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get __filename and __dirname in ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
@@ -10,18 +21,19 @@ app.use(express.json());
 // Middleware to enable CORS
 app.use(cors());
 
+// Serve frontend from the same server
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Use the auth routes
 app.use('/api/auth', authRoutes);
 
 // Use the protected route
 app.use('/api/user', protectedRoute);
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
 app.get('/healthcheck', (req, res) => {
-  res.status(200);
+  res.status(200).send('OK'); // Add send response
 });
 
 const port = process.env.PORT || 3001;
